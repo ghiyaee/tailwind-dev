267f62d76d8592ffe2653d77fefdf2d1fec5cdab
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-03-05 13:09:48 +0330

    change style menu

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,17 @@
 const menuHam = document.querySelector('#ham-menu');
-const menuOpen = document.querySelector('#menu')
+const menuOpen = document.querySelector('#menuBar');
 
 const handelToggel = () => {
-    menuOpen.classList.toggle('hidden');
-}
+  menuOpen.classList.toggle('hidden');
+  menuHam.classList.toggle('open')
+  menuOpen.classList.add('flex')
+
+};
 
 const handelCloseToggel = () => {
-    menuOpen.classList.toggle('hidden');
-    // menuOpen.classList.toggle('flex');
+  menuOpen.classList.toggle('hidden');
+  menuHam.classList.toggle('open');
+
 };
 
 menuHam.addEventListener('click', handelToggel);
